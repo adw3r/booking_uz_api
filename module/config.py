6003314266ccbcd395188de275bc4582163a54f8
alexey_naidiuk <alexeynaidiuk@gmail.com>
2023-07-06 17:19:46 +0300

    refactored config

--- module/config.py
+++ module/config.py
@@ -1,4 +1,3 @@
-import configparser
 import os
 import sys
 from pathlib import Path
@@ -6,21 +5,13 @@ from pathlib import Path
 import loguru
 from dotenv import load_dotenv
 
-config = configparser.ConfigParser()
 load_dotenv()
 
 ROOT_FOLDER = Path(__file__).absolute().parent.parent
-CONFIG_FILE = Path(ROOT_FOLDER, 'config.ini')
-config.read_file(CONFIG_FILE.open())
-URL = os.environ['URL']
-
-LOGGING_LEVEL = config['general'].getint('LOGGING_LEVEL')
 
 CAPMONSTER_HOST = os.environ.get('CAPMONSTER_HOST')
 CAPMONSTER_KEY = os.environ.get('CAPMONSTER_KEY')
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
-PROXIES_API_HOST = config['general']['PROXIES_API_HOST']
-
 
 logger = loguru.logger
 
